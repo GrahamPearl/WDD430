410fed51fcc8819575f7a6a3c16b440e1afd5482
PearlG <Graham.Pearl@gmail.com>
2021-05-01 19:13:38 +0200

    Update contact-list.component.ts

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -11,6 +11,7 @@ export class ContactListComponent implements OnInit {
   contacts: Contact[] = [
     new Contact('1','R. Kent Jackson','jacksonk@byui.edu','208-496-3771','../../../assets/images/jacksonk.jpg'),
     new Contact('2','Rex Barzee','barzeer@byui.edu','208-496-3768','../../../assets/images/barzeer.jpg'),
+    new Contact('3','G. Pearl','pea19005@byui.edu','081-476-2543','../../../assets/images/barzeer.jpg'),
   ];
   constructor() { }
 
