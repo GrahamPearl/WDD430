3d2a44c24b93c4aa0cca16b39c28396cbbfc2c7b
GrahamPearl <34150464+GrahamPearl@users.noreply.github.com>
2021-05-23 19:06:32 +0200

    W5 contacts as service

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,28 +1,30 @@
 import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
+  providers: [ ContactService ]
 })
 
 export class ContactListComponent implements OnInit {
   @Output() selectedContactEvent = new EventEmitter<Contact>();
 
-  contacts: Contact[] = [
-    new Contact('1','R. Kent Jackson','jacksonk@byui.edu','208-496-3771','../../../assets/images/jacksonk.jpg'),
-    new Contact('2','Rex Barzee','barzeer@byui.edu','208-496-3768','../../../assets/images/barzeer.jpg'),
-    new Contact('3','G. Pearl','pea19005@byui.edu','081-476-2543','../../../assets/images/barzeer.jpg'),
-  ];
-  constructor() { }
+  contacts: Contact[] = [];
+
+  constructor(private contactService : ContactService) { 
+  }
 
   ngOnInit(): void {
-    
+    this.contacts = this.contactService.getContacts();
   }
 
   onSelected(contact: Contact) {
     this.selectedContactEvent.emit(contact);
   }
+
+  
 
 }
